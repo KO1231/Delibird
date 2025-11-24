c2390f1f5b3d06debc8b13df101378ce2b99fd0f
Kazuhiro Oka <k.oka@kazutech.jp>
2025-11-25 01:19:23 +0900

    update: parser

--- lambda/redirect_request/app.py
+++ lambda/redirect_request/app.py
@@ -1,30 +1,31 @@
-import os
 from http import HTTPStatus
 
 from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent, event_source
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 from ddb.models.delibird_link import DelibirdLinkTableModel, DelibirdLinkInactiveStatus
-from parser import parse_request_path, parse_origin
+from parser import parse_request_path, parse_origin, parse_domain
 from query import queried_origin
 from util.logger_util import setup_logger
 from util.response_util import redirect_response, error_response
 
 logger = setup_logger("redirect_request")
-_ALLOWED_DOMAIN = set(str(os.environ["ALLOWED_DOMAIN"]).split(","))
 
 
 @event_source(data_class=APIGatewayProxyEvent)
 def lambda_handler(event: APIGatewayProxyEvent, context: LambdaContext):
-    # parse domain
-    domain = event.headers.get("Host")
-    if (not domain) or (domain not in _ALLOWED_DOMAIN):
-        logger.warning(f"Invalid or missing domain: {domain}")
+    try:
+        # parse domain
+        domain: str = parse_domain(event.headers.get("Host", ""))
+        # parse path
+        request_path: str = parse_request_path(event.path_parameters.get("path", ""))
+    except ValueError as e:
+        logger.info(f"Get Invalid request: {e}")
         return error_response(HTTPStatus.NOT_FOUND)
-    logger.info(f"Domain: {domain}")
-
-    # parse path
-    request_path: str = parse_request_path(event.path_parameters.get("path", ""))
+    except Exception:
+        logger.exception(f"Failed to parse request.")
+        return error_response(HTTPStatus.NOT_FOUND)
+    logger.info(f"Parsed Domain: {domain}, Parsed Request Path: {request_path}")
 
     # pathがない場合 "/"にアクセスされた場合は、NOT_FOUNDを返す
     if not request_path:
@@ -37,7 +38,7 @@ def lambda_handler(event: APIGatewayProxyEvent, context: LambdaContext):
 
     ## リンクが存在しない場合
     if link is None:
-        logger.info(f"Link(domain:{domain}, slug: {request_path}) does not exist or has been disabled.")
+        logger.info(f"Link(domain: {domain}, slug: {request_path}) does not exist or has been disabled.")
         return error_response(HTTPStatus.NOT_FOUND)
 
     ## リンクが無効な場合
@@ -46,30 +47,30 @@ def lambda_handler(event: APIGatewayProxyEvent, context: LambdaContext):
         inactive_status = inactive_reason.status
         if inactive_status is DelibirdLinkInactiveStatus.DISABLED:
             # 明示的に無効化されている場合
-            logger.info(f"Link(domain:{domain}, slug: {request_path}) is inactive. Reason: {inactive_reason}")
+            logger.info(f"Link(domain: {domain}, slug: {request_path}) is inactive. Reason: {inactive_reason}")
             return error_response(HTTPStatus.NOT_FOUND)
 
         elif inactive_status is DelibirdLinkInactiveStatus.EXPIRED:
             # 有効期限切れの場合
             if not link.expired_origin:
-                logger.info(f"Link(domain:{domain}, slug: {request_path}) is expired at {link.expiration_date}.")
+                logger.info(f"Link(domain: {domain}, slug: {request_path}) is expired at {link.expiration_date}.")
                 return error_response(HTTPStatus.NOT_FOUND)
             # リンク期限切れでexpired_originがあれば、そちらにリダイレクトする
             interrupt_origin = link.expired_origin
 
         elif inactive_status is DelibirdLinkInactiveStatus.MAX_USES_EXCEEDED:
             # 最大使用回数超過の場合
-            logger.info(f"Link(domain:{domain}, slug: {request_path}) has reached max uses: {link.uses} / {link.max_uses}.")
+            logger.info(f"Link(domain: {domain}, slug: {request_path}) has reached max uses: {link.uses} / {link.max_uses}.")
             return error_response(HTTPStatus.NOT_FOUND)
 
         else:
-            logger.error(f"Link(domain:{domain}, slug: {request_path}) is not active. Unknown reason: {inactive_reason}")
+            logger.error(f"Link(domain: {domain}, slug: {request_path}) is not active. Unknown reason: {inactive_reason}")
             return error_response(HTTPStatus.INTERNAL_SERVER_ERROR)
-    logger.info(f"Link(domain:{domain}, slug: {request_path}) is active. Current uses: {link.uses} / Max uses: {link.max_uses or "unlimited"}")
+    logger.info(f"Link(domain: {domain}, slug: {request_path}) is active. Current uses: {link.uses} / Max uses: {link.max_uses or "unlimited"}")
 
     # ステータスコードがリダイレクト系でない場合はInternal Server Errorを返す
     if not link.status.is_redirection:
-        logger.error(f"Link(domain:{domain}, slug: {request_path}) has invalid status code: {link.status}")
+        logger.error(f"Link(domain: {domain}, slug: {request_path}) has invalid status code: {link.status}")
         return error_response(HTTPStatus.INTERNAL_SERVER_ERROR)
 
     # リダイレクト先URLの決定
@@ -79,24 +80,24 @@ def lambda_handler(event: APIGatewayProxyEvent, context: LambdaContext):
         origin = parse_origin(origin_candidate)
     except Exception:
         # 不正なURLがデータベースに保存されている場合はInternal Server Errorを返す
-        logger.exception(f"Invalid URL stored in link_origin for domain:{domain}, slug: {request_path}, URL: {origin_candidate}")
+        logger.exception(f"Invalid URL stored in link_origin for domain: {domain}, slug: {request_path}, URL: {origin_candidate}")
         return error_response(HTTPStatus.INTERNAL_SERVER_ERROR)
 
     # クエリパラメータの付与
     if not link.query_omit:
-        logger.info(f"Link(domain:{domain}, slug: {request_path}) has disabled query omission. Appending query parameters.")
+        logger.info(f"Link(domain: {domain}, slug: {request_path}) has disabled query omission. Appending query parameters.")
         try:
             origin = queried_origin(origin, event.resolved_query_string_parameters, link.query_whitelist)
         except Exception:
-            logger.exception(f"Failed to append query parameters for domain:{domain}, slug: {request_path}, URL: {origin}")
+            logger.exception(f"Failed to append query parameters for domain: {domain}, slug: {request_path}, URL: {origin}")
             return error_response(HTTPStatus.INTERNAL_SERVER_ERROR)
 
     # リンクの使用回数をインクリメント
     try:
         link.increment_uses()
     except Exception:
-        logger.exception(f"Failed to increment uses for domain:{domain}, slug: {request_path}")
+        logger.exception(f"Failed to increment uses for domain: {domain}, slug: {request_path}")
         return error_response(HTTPStatus.INTERNAL_SERVER_ERROR)
 
-    logger.info(f"Redirect to {origin} for domain:{domain}, slug: {request_path} (status: {link.status})")
+    logger.info(f"Redirect to {origin} for domain: {domain}, slug: {request_path} (status: {link.status})")
     return redirect_response(origin, link.status)
--- lambda/redirect_request/parser.py
+++ lambda/redirect_request/parser.py
@@ -1,8 +1,11 @@
+import os
 from urllib.parse import quote
 from urllib.parse import urlparse
 
 # リダイレクト先URLで許可するスキーム
 _ALLOWED_REDIRECT_TARGET_SCHEMES = ["http", "https"]
+# リクエストの受け先ドメインの許可リスト
+_ALLOWED_DOMAIN = set([d.strip().lower() for d in str(os.environ["ALLOWED_DOMAIN"]).split(",")])
 
 
 def parse_request_path(path: str) -> str:
@@ -18,3 +21,11 @@ def parse_origin(url: str) -> str:
     if (not parsed.netloc):
         raise ValueError("URL must have a valid netloc.")
     return parsed.geturl()
+
+
+def parse_domain(domain: str) -> str:
+    """リクエストのドメインをパースして返す。"""
+    domain = domain.strip().lower()
+    if (not domain) or (domain not in _ALLOWED_DOMAIN):
+        raise ValueError(f"Invalid or missing domain: {domain}, allowed: {_ALLOWED_DOMAIN}")
+    return domain
