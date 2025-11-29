b155bd9bd2429821551c556fb6b36db14472b401
Kazuhiro Oka <k.oka@kazutech.jp>
2025-11-30 07:28:19 +0900

    update: parserをparse_utilに

--- lambda/redirect_request/app.py
+++ lambda/redirect_request/app.py
@@ -4,9 +4,9 @@ from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent, e
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 from ddb.models.delibird_link import DelibirdLinkTableModel, DelibirdLinkInactiveStatus
-from parser import parse_request_path, parse_origin, parse_domain
 from query import queried_origin
 from util.logger_util import setup_logger
+from util.parse_util import parse_request_path, parse_origin, parse_domain
 from util.response_util import redirect_response, error_response
 
 logger = setup_logger("redirect_request")
