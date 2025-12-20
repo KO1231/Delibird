706cb5ef44b3d0b222e8895494c0acbed73ba0b6
Kazuhiro Oka <k.oka@kazutech.jp>
2025-12-20 21:31:07 +0900

    add: error-protected.css

--- modules/aws_s3/stylesheet.tf
+++ modules/aws_s3/stylesheet.tf
@@ -13,6 +13,21 @@ resource "aws_s3_object" "error_css" {
   etag = filemd5("${local.local_base_path}/css/error.css")
 }
 
+resource "aws_s3_object" "error_protected_css" {
+  # アップロード元(ローカル)
+  source = "${local.local_base_path}/css/error-protected.css"
+
+  # アップロード先(S3)
+  bucket = var.bucket_name
+  key    = "${var.remote_base_path}/css/error-protected.css"
+
+  # Content-Type の設定
+  content_type = "text/css"
+
+  # エンティティタグ (ファイル更新のトリガーに必要)
+  etag = filemd5("${local.local_base_path}/css/error-protected.css")
+}
+
 resource "aws_s3_object" "admin_portal_css" {
   # アップロード元(ローカル)
   source = "${local.local_base_path}/css/admin-portal.css"
